5e35b9ed6d27dc4e4d7f25801795a1ee7e3032bb
Hugo Machado <piufop@gmail.com>
2017-09-14 18:46:30 -0300

    Creates chunks and get content type id from plan

---
+++ test/unit/lib/content-type-ids-in-plan.spec.js
@@ -0,0 +1,45 @@
+const ROOT_DIR = process.cwd();
+const { expect } = require('chai');
+
+const contentTypeIdsInPlan = require(`${ROOT_DIR}/lib/content-type-ids-in-plan`);
+
+describe('content-type-ids-in-plan', () => {
+  it('get content type ids from plan', () => {
+    const plan = [
+      [
+        {
+          "type": "contentField/copy",
+          "meta": {
+            "contentTypeInstanceId": "content/song/0",
+            "fieldInstanceId": "fields/song/newName/0"
+          },
+          "payload": {
+            "contentTypeId": "song",
+            "fromId": "song",
+            "toId": "newName"
+          }
+        }
+      ],
+      [
+        {
+          "type": "contentField/copy",
+          "meta": {
+            "contentTypeInstanceId": "content/song1/0",
+            "fieldInstanceId": "fields/song1/newName2/0"
+          },
+          "payload": {
+            "contentTypeId": "song1",
+            "fromId": "song1",
+            "toId": "newName2"
+          }
+        }
+      ]
+    ];
+
+    const contentTypeIds = contentTypeIdsInPlan(plan);
+
+    expect(contentTypeIds).to.be.a('Array');
+    expect(contentTypeIds).to.have.length(2);
+    expect(contentTypeIds).to.deep.equal(['song', 'song1']);
+  });
+});

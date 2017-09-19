e9721c06db99bb168531e9176d612ade4e57ddd1
Hugo Machado <piufop@gmail.com>
2017-09-19 15:09:03 -0300

    Add transformation feature

--- test/unit/lib/content-migration-payloads.spec.js
+++ test/unit/lib/content-migration-payloads.spec.js
@@ -225,4 +225,108 @@ describe('content-migration-payloads', () => {
       }
     }]);
   });
+
+  it('transform entries', () => {
+    const plan = [
+      [
+        {
+          type: "content/transform",
+          transform: (entry) => {
+            Object.keys(entry.fields.title).forEach((key) => {
+              entry.fields['new-title'][key] = entry.fields.title[key].slice(0, 5);
+            });
+            return entry;
+          },
+          meta: {
+            contentTypeInstanceId: "content/song/0"
+          },
+          payload: {
+            contentTypeId: "song"
+          }
+        }
+      ]
+    ];
+
+    const entries = [
+      {
+        "sys": {
+          "space": {
+            "sys": {
+              "type": "Link",
+              "linkType": "Space",
+              "id": "tjj66gb8nfwn"
+            }
+          },
+          "id": "2DiUM9ZZMsIGKsM08YYQge",
+          "type": "Entry",
+          "createdAt": "2017-09-07T19:50:04.231Z",
+          "updatedAt": "2017-09-07T19:51:07.532Z",
+          "createdBy": {
+            "sys": {
+              "type": "Link",
+              "linkType": "User",
+              "id": "4EHGw9NLlQYCVy1RY5kHuI"
+            }
+          },
+          "updatedBy": {
+            "sys": {
+              "type": "Link",
+              "linkType": "User",
+              "id": "4EHGw9NLlQYCVy1RY5kHuI"
+            }
+          },
+          "publishedCounter": 1,
+          "version": 8,
+          "publishedBy": {
+            "sys": {
+              "type": "Link",
+              "linkType": "User",
+              "id": "4EHGw9NLlQYCVy1RY5kHuI"
+            }
+          },
+          "publishedVersion": 7,
+          "firstPublishedAt": "2017-09-07T19:51:07.532Z",
+          "publishedAt": "2017-09-07T19:51:07.532Z",
+          "contentType": {
+            "sys": {
+              "type": "Link",
+              "linkType": "ContentType",
+              "id": "song"
+            }
+          }
+        },
+        "fields": {
+          "title": {
+            "en-US": "Bacon Cru"
+          },
+          "new-title": {
+            "en-US": ""
+          }
+        }
+      }
+    ];
+
+    const payloads = contentMigrationPayloads(plan, entries);
+
+    expect(payloads).to.be.a('Array');
+    expect(payloads).to.have.length(1);
+    expect(payloads).to.have.deep.equal([{
+      "payload": {
+        "fields": {
+          "title": {
+            "en-US": "Bacon Cru"
+          },
+          "new-title": {
+            "en-US": "Bacon"
+          }
+        }
+      },
+      "meta": {
+        "version": 8,
+        "entryId": "2DiUM9ZZMsIGKsM08YYQge",
+        "contentTypeId": "song"
+      }
+    }]);
+  });
+
 });

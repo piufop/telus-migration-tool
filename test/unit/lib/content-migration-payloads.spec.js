268c9250d688374f3edc790ad52489c23b5c8736
Hugo Machado <piufop@gmail.com>
2017-09-21 13:35:34 -0300

    Adds test to transform without locale

--- test/unit/lib/content-migration-payloads.spec.js
+++ test/unit/lib/content-migration-payloads.spec.js
@@ -592,7 +592,7 @@ describe('content-migration-payloads', () => {
     }]);
   });
 
-  it('applies transform to a content', () => {
+  it('applies transform to a content with locale', () => {
     const plan = [
       [
         {
@@ -683,6 +683,99 @@ describe('content-migration-payloads', () => {
           "new-title": {
             "en-US": "Ba"
           }
+        }
+      },
+      "meta": {
+        "version": 8,
+        "entryId": "2DiUM9ZZMsIGKsM08YYQge",
+        "contentTypeId": "song"
+      }
+    }]);
+  });
+
+  it('applies transform to a content without locale', () => {
+    const plan = [
+      [
+        {
+          "type": "contentField/copy",
+          "meta": {
+            "contentTypeInstanceId": "content/song/0",
+            "fieldInstanceId": "fields/title/new-title/0"
+          },
+          "payload": {
+            "contentTypeId": "song",
+            "transform": (content) => (content.slice(0, 2)),
+            "fromId": "title",
+            "toId": "new-title"
+          }
+        }
+      ]
+    ];
+
+    const entries = [
+      {
+        "sys": {
+          "space": {
+            "sys": {
+              "type": "Link",
+              "linkType": "Space",
+              "id": "tjj66gb8nfwn"
+            }
+          },
+          "id": "2DiUM9ZZMsIGKsM08YYQge",
+          "type": "Entry",
+          "createdAt": "2017-09-07T19:50:04.231Z",
+          "updatedAt": "2017-09-07T19:51:07.532Z",
+          "createdBy": {
+            "sys": {
+              "type": "Link",
+              "linkType": "User",
+              "id": "4EHGw9NLlQYCVy1RY5kHuI"
+            }
+          },
+          "updatedBy": {
+            "sys": {
+              "type": "Link",
+              "linkType": "User",
+              "id": "4EHGw9NLlQYCVy1RY5kHuI"
+            }
+          },
+          "publishedCounter": 1,
+          "version": 8,
+          "publishedBy": {
+            "sys": {
+              "type": "Link",
+              "linkType": "User",
+              "id": "4EHGw9NLlQYCVy1RY5kHuI"
+            }
+          },
+          "publishedVersion": 7,
+          "firstPublishedAt": "2017-09-07T19:51:07.532Z",
+          "publishedAt": "2017-09-07T19:51:07.532Z",
+          "contentType": {
+            "sys": {
+              "type": "Link",
+              "linkType": "ContentType",
+              "id": "song"
+            }
+          }
+        },
+        "fields": {
+          "title": "Bacon Cru",
+          "new-title": ""
+        }
+      }
+    ];
+
+    const payloads = contentMigrationPayloads(plan, entries);
+
+    expect(payloads).to.be.a('Array');
+    expect(payloads).to.have.length(1);
+    expect(payloads).to.have.deep.equal([{
+      "payload": {
+        "fields": {
+          "title": "Bacon Cru",
+          "new-title": "Ba"
         }
       },
       "meta": {
